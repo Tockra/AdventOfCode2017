8c5ded8dab03bf93cee640fa49a6accdd0340206
Tockra <tim.tannert@tu-dortmund.de>
2017-01-21 23:51:01 +0100

    day05_02 abgeschlossen + einige aenderungen an _01

--- day05_02/src/main.rs
+++ day05_02/src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 		let mut hasher = Md5::new();
 		
 		// Vektor für die Ausgabe
-		let mut output = Vec::new();
+		let mut output = ['-';8];
 		
 		// i ist die aktuelle Erweiterung des Wortes line : line + id
 		// i wird anschließend solange erhöht, bis ein MD5 Hash mit 5 führenden Nullen gefunden wurde
@@ -26,21 +26,26 @@ fn main() {
 			  let mut result = [0; 16]; 
 		      hasher.result(&mut result);
 		      
-		      // Idee für diese Optimierung von : https://gist.github.com/gkbrk/2e4835e3a17b3fb6e1e7
+		      // Idee für diese Optimierung von https://gist.github.com/gkbrk/2e4835e3a17b3fb6e1e7 übernommen
 		      let first_five = result[0] as i32 + result[1] as i32 + (result[2] >> 4) as i32;
 		      
 		      if first_five == 0 {
-				  output.push(hasher.result_str().chars().nth(5).unwrap());
+			      // Die entsprechende Stelle des Ergebnisses wird nur gesetzt, falls sie zuvor noch nicht gesetzt wurde
+			      // und die angegebene Stelle gültig ist
+			      if hasher.result_str().chars().nth(5).unwrap().to_digit(16).unwrap() <8 && output[hasher.result_str().chars().nth(5).unwrap().to_digit(16).unwrap() as usize] == '-' {
+					  output[hasher.result_str().chars().nth(5).unwrap().to_digit(16).unwrap() as usize] = hasher.result_str().chars().nth(6).unwrap();
+			      }
 		      }
 		      hasher.reset(); 
 		      
 		      // Wenn die Ausgabe 8 Zeichen lang ist, kann abgebrochen werden
-		      if output.len() ==8 {
-				  break;
-		      } 
+		     if output.iter().all(|&x| x != '-') {
+		     	break;
+		     }
+		    
 		}
 		// Ausgabe
-		println!("{}",output.into_iter().collect::<String>());
+		println!("{}",output.iter().cloned().collect::<String>());
 	
 	}
 	
