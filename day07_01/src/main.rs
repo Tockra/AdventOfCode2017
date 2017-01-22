075e62311b63016f4224174f3d2b4799f11017a1
Tockra <tim.tannert@tu-dortmund.de>
2017-01-22 20:45:53 +0100

    day07_01 angelegt

---
+++ day07_01/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+	
+	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
+	// ist nicht notwendig.
+	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
+	for line in include_str!("../input.data").lines() {
+		
+	}
+	
+	
+	// Ausgabe
+	
+}
+
+
