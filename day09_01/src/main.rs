fdba675228d4d99318a0c7b5f1476e9050ab33b7
Tockra <tim.tannert@tu-dortmund.de>
2017-01-26 11:39:32 +0100

    fix

--- day09_01/src/main.rs
+++ day09_01/src/main.rs
@@ -15,7 +15,10 @@ fn main() {
 	for line in include_str!("../input.data").lines() {
 		// Eingabe wird geparsed und in einen Vector mit Input_Elems mgewandelt
 		let mut input: Vec<Input_Elems> = Vec::new();
-		let tmp = line.split(|c| ['[', ']'].contains(&c));
+		let tmp = line.split(|c| ['(', ')'].contains(&c));
+		for elem in tmp.enumerate() {
+			
+		}
 		// Es wird durch den String iteriert. Wenn in einem Block (axb) eingetreten wird, dann werden die nächsten a Zeichen b mal ins Ergebniss aufgenommen. 
 		//Dann werden a Zeichen übersprungen und weiter gezählt
 		for c in line.replace('(',"").replace(')',"").replace('x',",").chars() {
