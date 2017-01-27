e083cf2c5cff1f4a75b427788338abde9cc61604
Tockra <tim.tannert@tu-dortmund.de>
2017-01-27 23:21:12 +0100

    day05_02 abgeschlossen + einige aenderungen an _01

--- day10_01/src/main.rs
+++ day10_01/src/main.rs
@@ -17,21 +17,51 @@ impl Bot {
 	
 	// Prüft ob der Bot einen higher Value besitzt
 	fn has_higher(&self) -> bool {
-		self.higher == 0
+		self.higher != 0
 	}
 	
 	// Prüft ob der Bot einen lower Value besitzt
 	fn has_lower(&self) -> bool {
-		self.lower == 0
+		self.lower != 0
 	}
 	
-	// Funktion um einem Bot einen Wert zu geben
+	// Funktion um einem Bot einen Chip gibt. Dabei wird darauf geachtet, dass der Wert im lower immer kleiner als der 
+	// im higher ist
 	fn give_value(&mut self, value: u32) {
-		if self.has_lower() {
-			self.higher = value;
+		if (self.lower == 61 && value == 17) || (self.lower == 17 && value == 61) {
+			println!("Huhu");
+		}
+		if self.lower > value {
+			self.higher = self.lower;
+			self.lower = value;
 		}
 		else {
 			self.lower = value;
+		}
+	}
+	
+	// Diese Funktion prüft, ob sowohl higher und lower voll sind. 
+	// Ist dies der Fall, werden die Chips weitergegeben
+	fn interact(&self, mut bots: &mut [Bot], mut outputs: &mut [u32]) {
+		if self.has_higher() && self.has_lower() {
+			println!("lower: {}, higher: {}",self.lower,self.higher);
+			if self.bot_lower != 0 {
+				let mut bot = bots[self.bot_lower];
+				bot.give_value(self.lower);
+				bot.interact(bots, outputs);
+			}
+			else {
+				outputs[self.output_lower] = self.lower;
+			}
+			
+			if self.bot_higher != 0 {
+				let mut bot = bots[self.bot_higher];
+				bot.give_value(self.higher);
+				bot.interact(bots, outputs);
+			}
+			else {
+				outputs[self.output_higher] = self.higher;
+			}
 		}
 	}
 }
@@ -40,10 +70,6 @@ impl Bot {
 // wird bei "value 11 goes to bot 124" beispielsweise immer das 2. und 6. Wort genommen, um die Bot-NR
 // und den Wert zu ermitteln!
 fn main() {
-	
-	// Erzeugt einen Vektor, der alle Zeilen beinhaltet, die beim ersten durchlauf geskippt werden und erst beim zweiten 
-	// durchlauf betrachtet werden
-	let mut skipped = Vec::new();
 	
 	// Array in dem jeder Index der entsprechenden Botnummer entspricht
 	let mut bots = [Bot::new();500];
@@ -68,38 +94,40 @@ fn main() {
 			bots[tmp_it.nth(3).unwrap().parse::<usize>().unwrap()].give_value(value);
 		}
 		else {
-			skipped.push(line);
+			// Für ein besseres verständnis werden einige Hilfsvariabeln definiert
+			let mut iterator = line.split(" ");
+			let bot_id = iterator.nth(1).unwrap().parse::<usize>().unwrap();
+			let low_typ = iterator.nth(3).unwrap();
+			let low_val = iterator.nth(0).unwrap().parse::<usize>().unwrap();
+			let high_typ = iterator.nth(3).unwrap();
+			let high_val = iterator.nth(0).unwrap().parse::<usize>().unwrap();
+			
+			let val_lower = bots[bot_id].lower as usize;
+			
+			// Dem Bot bot_id wird gesagt, an wen der lower-Chip geht
+			if low_typ.contains("bot") {
+				bots[low_val].bot_lower = val_lower;
+			}
+			else { // Output
+				bots[low_val].output_lower = val_lower;
+			}
+			
+			let val_higher = bots[bot_id].higher as usize;
+			// Dem Bot bot_id wird gesagt, an wen der higher-Chip geht
+			if high_typ.contains("bot") {
+				bots[high_val].bot_higher = val_higher;
+			}
+			else { // Output
+				bots[high_val].output_higher = val_higher;
+			}
 		}
 	}
 	
-	for line in skipped.into_iter() {
-		// Für ein besseres verständnis werden einige Hilfsvariabeln definiert
-		let mut tmp_it = line.split(" ");
-		let bot_id = tmp_it.nth(1).unwrap().parse::<usize>().unwrap();
-		let low_typ = tmp_it.nth(3).unwrap();
-		let low_val = tmp_it.nth(0).unwrap().parse::<usize>().unwrap();
-		let high_typ = tmp_it.nth(3).unwrap();
-		let high_val = tmp_it.nth(0).unwrap().parse::<usize>().unwrap();
-		
-		let val_lower = bots[bot_id].lower as usize;
-		if low_typ.contains("bot") {
-			bots[low_val].bot_lower = val_lower;
-		}
-		else { // Output
-			bots[low_val].output_lower = val_lower;
-		}
-		
-		let val_higher = bots[bot_id].higher as usize;
-		if high_typ.contains("bot") {
-			bots[high_val].bot_higher = val_higher;
-		}
-		else { // Output
-			bots[high_val].output_higher = val_higher;
-		}
-		
+	// Durchlaufe alle Bots und lasse alle Bots mit 2 Werten ihre Instruktionen durchführen
+	for i in 0..bots.len() {
+	    let bot = bots[i].clone();
+	    bot.interact(&mut bots,&mut outputs);
 	}
-	
-	
 	
 	
 	// Ausgabe
