1b5d7436d3035d3c17309315390082140687b7bb
Tockra <tim.tannert@tu-dortmund.de>
2017-02-02 11:11:28 +0100

    day11_01 erzeugt

---
+++ day11_01/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+	
+	// Array mit einem Vector für jede Etage (von 0-3)
+	//let mut floors = 
+	
+	// Aktuelle Aufzugsposition
+	let curr_elevator_pos: u8 = 0;
+	
+	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
+	// ist nicht notwendig.
+	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
+	for line in include_str!("../input.data").lines() {
+		
+	}
+	
+	
+	// Ausgabe
+	
+	
+	
+}
