7c8fa035b1c4c7b3d630088e9f97315c26b5d9a3
Tim McTa <tim.tannert@tu-dortmund.de>
2017-12-01 09:26:13 +0100

    Tag1_01 von 2016 auf 2017 geändert

--- day01_01/src/main.rs
+++ day01_01/src/main.rs
@@ -1,40 +1,7 @@
-enum Direction {
-	North,
-	East,
-	South,
-	West,
-}
 
-impl Direction {
-	// Diese Funktion bestimmt ausgehend von der alten Ausrichtung und der aktuellen Bewegung, die neue Ausrichtung
-	fn compute_direction(self,turn:char) -> Direction {
-		if turn == 'R' {
-			match self {
-				Direction::North => Direction::East,
-				Direction::East => Direction::South,
-				Direction::South => Direction::West,
-				Direction::West => Direction::North,
-			}
-		}
-		else {
-			match self {
-				Direction::North =>  Direction::West,
-				Direction::East =>  Direction::North,
-				Direction::South =>  Direction::East,
-				Direction::West =>  Direction::South,
-			}
-		}
-	}
-}
+fn main() {	
+	let mut list: Vec<u32> = vec![];
 
-
-fn main() {
-	// Koordinaten
-	let mut x: i16 = 0;
-	let mut y: i16 = 0;
-	let mut direction = Direction::North;
-	
-	
 	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
 	// ist nicht notwendig.
 	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
@@ -42,33 +9,34 @@ fn main() {
 		// In dieser Aufgabe existiert nur eine Zeile, somit ist diese Schleife nicht zwingend notwendig. Sie wird aber für die folgenden
 		// Aufgaben beibehalten.
 		
-		for elem in line.replace(' ', "").split(",") {
-			// Die Richtiung in die gedreht werden soll
-			let turn:char = elem.chars().nth(0).unwrap();
-			// Die Distanz die gegangen werden soll. Dafür reicht ein i16 !
-			let distance:i16 = (&elem[1..]).parse().unwrap();
-		
-			// Neue Richtung wird bestimmt:
-			direction = direction.compute_direction(turn);
-		
-			// Neue Position wird bestimmt:
-			compute_new_pos(&mut x,&mut y,distance,&direction);
-		}
+		// Überführt die Line bestehend aus einer zusammenhängenden Zahl in einen Vektor, bei dem jeder Eintrag einem Zeichen/Digit der Zahl entspricht.
+		list = line.trim().chars().map(|c| c.to_digit(10).unwrap()).collect();
 
-		//Finale Ausgabe
-		println!("X: {}, Y: {} Distanz: {}",x,y,x.abs()+y.abs());
 	}
 
-}
 
-// Diese Funktion erhält 2 veränderbare Referenzen auf die X und Y Koordinate, die Distanz die gelaufen werden soll und eine Referenz auf 
-// die aktuelle Richtung. Anschließend besitzen die x- und y-Veriabeln die richtigen Werte!
-fn compute_new_pos(x: &mut i16,y: &mut i16,distance: i16, d: &Direction) {
-	match *d {
-		Direction::North => (*y += distance),
-		Direction::East => (*x += distance),
-		Direction::South => (*y -= distance),
-		Direction::West => (*x -= distance),
-	};
+	// Summe die als Ergebnis dient
+	let mut sum = 0;
+
+	// Durchläuft jede zahl und prüft, ob diese ihrem Vorgänger entspricht. Falls dies der Fall ist, wird die Summe erhöht
+	for i in 0..list.len() {
+		// Sonderfall für erstes und letztes Element:
+
+		if i == 0 {
+			if list[i] == list[list.len()-1] {
+				sum += list[i];
+			}
+		}
+		else {
+			if list[i] == list[i-1] {
+				sum += list[i];
+			}
+		}
+
+	}
+
+	println!("Das Ergebnis dieser Aufgabe entspricht: {}",sum);
+
+
 }
 
