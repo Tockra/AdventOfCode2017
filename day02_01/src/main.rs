b168ecb1932bc4817ae2056fed6b6b0d6fd188c6
Tim McTa <tim.tannert@tu-dortmund.de>
2017-12-02 12:10:15 +0100

    day02_01 2017 gelöst

--- day02_01/src/main.rs
+++ day02_01/src/main.rs
@@ -1,43 +1,32 @@
 fn main() {
-	
-	// Feld, dass das Keypad darstellt, -1 Felder sind nicht betretbare Felder
-	let mut keypad = [[-1;11];11];
-	
-	// Aktuelle Position im Keypad
-	let mut x: usize = 5;
-	let mut y: usize = 5;
-	
-	//Array für Aufgabe 2_01 definieren, so dass die inneren Symbole den Tasten des Keypads entsprechen
-	keypad[4][4] = 1; keypad[5][4] = 2; keypad[6][4] = 3;
-	keypad[4][5] = 4; keypad[5][5] = 5; keypad[6][5] = 6;
-	keypad[4][6] = 7; keypad[5][6] = 8; keypad[6][6] = 9;
-	
 
-	
-	
+	// Ergebnissumme
+	let mut sum = 0;
+
 	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
 	// ist nicht notwendig.
 	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
 	for line in include_str!("../input.data").lines() {
+		// Jede Zeile wird durchlaufen und min und max werden berechnet
+		let mut min: i32 = line.split_whitespace().nth(0).unwrap().parse().unwrap();
+		let mut max: i32 = line.split_whitespace().nth(0).unwrap().parse().unwrap();
 		
-		// Jede Zeile gibt eine Wegbeschreibung (char für char) zum Zielkey aus. Nach abarbeitung der folgenden Schleife 
-		// entsprechen x und y den Koordinaten des Zielkeys.
-		for elem in line.chars() {
-			match elem {
-				'R' if keypad[x+1][y] != -1 => x +=1,
-				'L' if keypad[x-1][y] != -1 => x -=1,
-				'U' if keypad[x][y-1] != -1 => y -=1,
-				'D' if keypad[x][y+1] != -1 => y +=1,
-				_ => ()
-				
-			}
+		for elem in line.split_whitespace() {
 
+			let curr_val = elem.parse().unwrap();
+			if curr_val > max {
+				max = curr_val;
+			}
+			if curr_val < min {
+				min = curr_val;
+			}
 		}
-		
-		// Zielkey wird ausgegeben
-		println!("Aktuelle Zahl: {}",keypad[x][y]);
-		
+	
+		sum += (max-min).abs();
+	
 	}
+
+	println!("Die gesuchte Summe beträgt: {}",sum);
 
 }
 
