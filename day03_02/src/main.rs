46b8da81cd0789535d968cbe912be20b8179f579
Tim McTa <tim.tannert@tu-dortmund.de>
2017-12-04 15:29:16 +0100

    day03_02 hässlige, aber richtige Lösung nachgereicht

--- day03_02/src/main.rs
+++ day03_02/src/main.rs
@@ -1,82 +1,86 @@
-// Datenstruktur zur simplen Darstellung eines Dreiecks
-#[derive(Copy,Clone)]
-struct Triangle {
-	x: u16,
-	y: u16,
-	z: u16
+// Datenstruktur zur Beschreibung der 4 Schritte die gemacht werden dürfen
+#[derive(Clone, Copy, Debug)]
+enum Step {
+    UP,
+	DOWN,
+	RIGHT,
+	LEFT,
 }
 
-impl Triangle {
-	fn new() -> Triangle {
-        Triangle { x: 0, y: 0, z: 0, }
-    }
-    
-    // Funktion, mit deren Hilfe ein Dreieck Seite für Seite definiert werden kann
-	fn fill(&mut self, val: u16) {
-		if self.y != 0  {
-			self.z = val;
-		}
-		else if self.x != 0 {
-			self.y = val;
-		}
-		else {
-			self.x = val;
-		}
-	}
-	
-	fn is_complete(&self) -> bool {
-		self.x != 0 && self.y != 0 && self.z != 0
-	}
-	
-	// Nach der gegebenen Bedingung wird geprüft, ob das aktuelle Dreieck ein echtes Dreieck sein kann
-	fn is_triangle(&self) -> bool {
-		self.x + self.y > self.z && self.x + self.z > self.y && self.y + self.z > self.x
+fn get_next_direction(d: &Step) -> Step {
+	match *d {
+		Step::UP => Step::LEFT,
+		Step::DOWN => Step::RIGHT,
+		Step::RIGHT => Step::UP,
+		Step::LEFT => Step::DOWN
 	}
 }
 
+fn get_next_coords(curr_dir: &Step, x: usize, y: usize) -> (usize,usize) {
+	match *curr_dir {
+		Step::UP => (x,y-1),
+		Step::DOWN => (x,y+1),
+		Step::RIGHT => (x+1,y),
+		Step::LEFT => (x-1,y),
+	}
+}
+
+// Ränder des Arrays werden ignoriert. Gibt es keine gültige Lösung, crasht das Programm :p
+fn generate_spiral(max_val: i32) -> i32 {
+	// Hardcoded 20x20 Spirale
+	let mut spiral = [[0; 20]; 20];
+
+	let mut curr_x: usize = 10;
+	let mut curr_y: usize = 10;
+
+	let mut curr_dis = 1;
+	let mut curr_direction = Step::RIGHT;
+	let mut twice = false;
+	spiral[10][10] = 1;
+	for i in 1..max_val {
+		let (x,y) = get_next_coords(&curr_direction,curr_x,curr_y);
+		curr_x = x;
+		curr_y = y;
+		
+		// Das Neue Feld entspricht der Summe aller Nachbarn. Da alle noch nicht initialisierten Nachbarn = 0 sind, muss hier keine Fallunterscheidung gemacht werden.
+		spiral[curr_x][curr_y] = spiral[curr_x-1][curr_y] + spiral[curr_x][curr_y-1] + spiral[curr_x][curr_y+1] + spiral[curr_x+1][curr_y] + spiral[curr_x+1][curr_y+1] + spiral[curr_x-1][curr_y-1] + spiral[curr_x-1][curr_y+1]+ spiral[curr_x+1][curr_y-1];      
+		
+		if spiral[curr_x][curr_y] > max_val {
+			return spiral[curr_x][curr_y]
+		}
+		// Zweimal bereits curr_dis erreicht. Somit muss die Richtung gewechselt werden
+		if i%curr_dis == 0 && twice {
+
+			// Richtungswechsel
+			curr_dis += 1;
+			curr_direction = get_next_direction(&curr_direction);
+			twice = false;
+		}
+		else if i%curr_dis == 0 {
+			curr_direction = get_next_direction(&curr_direction);
+			twice = true;
+		}
+		
+		
+	}
+	0
+}
 
 fn main() {
-	// Anzahl der gefundenen möglichen Dreiecke
-	let mut count = 0;
-	
-	
-	// Es werden immer 3 Dreiecke aufeinmal eingelesen
-	let mut dreiecke = [Triangle::new();3];
-	
+
 	
 	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
 	// ist nicht notwendig.
 	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
 	for line in include_str!("../input.data").lines() {
-		// Zähler für das 1., 2. und 3. Dreieck
-		let mut i = 0;
+		// Wie bereits in der ersten Aufgabe existiert lediglich eine Zeile zum Einlesen. Die Schleife wird auf Grund der Ähnlichkeit zu den anderne Aufgaben denoch nicht angepasst!
+		let spiral = generate_spiral(line.parse::<i32>().unwrap());
 		
-		// Jede Zeile beinhaltet ein Dreieck. Die einzelnen Koordinaten sind mit Leerzeichen getrennt
-		for elem in line.split_whitespace() {
-			//Koordinate wird zu u16 geparsed und in das aktuelle Dreieck gepusht
-			dreiecke[i].fill(elem.parse().unwrap());
-			i += 1;
-		}
-
-		
-		// Wenn das 0. Dreieck komplett ist, dann sind an dieser Stelle auch das 2. und 3. Dreieck komplett, da immer 3 Koordinaten pro Zeile eingelesen werden.
-		if dreiecke[0].is_complete() {
-			
-			// Prüfe ob das 1., 2. und 3. Dreieck echte Dreiecke sind
-			for dr in dreiecke.iter() {
-				if dr.is_triangle() {
-					count += 1;
-				}
-			}
-			
-			// Die drei aktuell betrachteten Dreiecke werden zurückgesetzt
-			dreiecke = [Triangle::new();3];
-		}
+		println!("Die erste zahl größer als {} ist {}.",line,spiral);
 		
 	}
 	
-	// Ausgabe der Anzahl der möglichen Dreiecke
-	println!("Mögliche Dreiecke: {}",count);
+
 
 }
 
