08d98797403ae6baf97293265422d9449af1a377
Tim McTa <tim.tannert@tu-dortmund.de>
2017-12-07 08:32:06 +0100

    day07_02 beendet 2017

--- day07_02/src/main.rs
+++ day07_02/src/main.rs
@@ -1,53 +1,84 @@
+use std::collections::HashMap;
+
 fn main() {
-	//Anzahl der Abbanyme
-	let mut count = 0;
+	//Name des Programms das auf einer linken Seite, aber auf keiner rechten Seite vorkommt.
+	let mut root = "";
+
+	// Kostenfunktion costs(p) für Programme:
+	let mut costs = HashMap::new();
 	
+	let mut children = HashMap::new();
+
 	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
 	// ist nicht notwendig.
 	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
 	for line in include_str!("../input.data").lines() {
+		let prog = line.split_whitespace().nth(0).unwrap();
+		let weight = line.split_whitespace().nth(1).unwrap().replace("(","").replace(")","").parse::<i32>().unwrap();
 		
-		let (even,odd) : (Vec<(usize,&str)>,Vec<(usize,&str)>) = line.split(|c| ['[', ']'].contains(&c)).enumerate().partition(|&n| n.0 % 2 == 0);
-		
-		// Prüft, ob es irgendwo im super_net-Teil ein Aba mit zugehörigem Bab aus dem hyper_net Teil gibt
-		if even.into_iter().any(|x| contains_aba(x.1,&odd)) {
-			count += 1 ;
-		}
-		
-	}
-	
-	// Ausgabe
-	println!("Es wurden {} gültige Addressen gefunden!",count);
-}
+		// Befüllt kosten
+		costs.insert(prog,weight);
 
-// Prüft ob addr_part ein Aba mit zugehörigen Bab beinhaltet. Es wird davon ausgegangen, dass 1 char = 1 byte
-fn contains_aba(addr_part: &str, hyper_net: &Vec<(usize,&str)>) -> bool {
-	for limit_left in 0..addr_part.len()-2 {
-		
-		// Es wird davon ausgegenangen, dass nur normale nicht Unicode-Buchstaben in addr_teil stehen
-		let slice = &addr_part[limit_left..limit_left+3];
-		
-		// Wenn ein aba gefunden wird, dann wird geschaut, ob in irgendeinem zugehörigen hyper_net teil ein Bab existiert
-		if is_aba(slice) && contains_bab(slice,hyper_net) {
-			return true;
+		// Befüllt Kinder
+		if line.contains("->") {
+			let c = line.split("-> ").nth(1).unwrap().split(", ").collect::<Vec<&str>>();
+			children.insert(prog, c);
+		}
+		else {
+			children.insert(prog,vec![]);
+		}
+
+
+		if count_program(&include_str!("../input.data").lines().collect(), prog) < 2 {
+			root = prog;
 		}
 	}
-	false
-}
-
-fn contains_bab(aba: &str, hyper_net: &Vec<(usize,&str)>) -> bool {
-	// bab Zeichenkette wird erzeugt
-	let mut bab = String::new();
-	bab.push(aba.chars().nth(1).unwrap());
-	bab.push(aba.chars().nth(0).unwrap());
-	bab.push(aba.chars().nth(1).unwrap());
 	
-	// Es wird geprüft, ob irgendein hyper_net Teil die Bab Zeichenkette beinhaltet
-	hyper_net.iter().any(|x| x.1.to_string().contains(&bab))
-}
-// Funktion prüft, ob ein übergegebener 3 elementiger String ein Abbanym ist
-// Es wird sichergestellt das kein unwrap jemals realistisch panict
-fn is_aba(candidate: &str) -> bool {
-	candidate.chars().nth(0).unwrap() ==candidate.chars().nth(2).unwrap()  && candidate.chars().nth(0).unwrap()  != candidate.chars().nth(1).unwrap() 
+	check_balance(root,&costs, &children);
+	
 }
 
+fn count_program(rest: &Vec<&str> , prog: &str) -> i32 {
+	let mut count = 0;
+	for progg in rest {
+		if progg.to_string().contains(prog) {
+			count += 1;
+		}
+	}
+	count
+}
+
+fn check_balance(root: &str, costs: &HashMap<&str,i32>, adj: &HashMap<&str,Vec<&str>>) -> i32 {
+	let mut child_weight: Vec<i32> = vec![];
+
+	for c in adj.get(root).unwrap() {
+		let result = check_balance(c,&costs, &adj);
+		if result == -1 {
+			return -1;
+		}
+		child_weight.push(check_balance(c,&costs, &adj));
+	}
+
+    // Bei dem Ergebnis muss man etwas raten, welches Gewicht das ist, dass am tiefsten im Baum ist. 
+	// In der Regel muss das aber das kleinste sein.
+	if !child_weight.iter().all(|&x| x == child_weight[0]) {
+		for i in 0..child_weight.len() {
+
+			// Element das anders ist als alle anderen
+			if child_weight.iter().filter(|&&x| x == child_weight[i]).count() != child_weight.len()-1 {
+				// Berechnet den Wert, um den das Gewicht verändert werden muss
+				let diff = child_weight[i] - child_weight[(i+1)%child_weight.len()];
+
+				// Gebe neues Soll-Gewicht für Programm aus
+				println!("Das Gewicht von {} muss auf {} gesetzt werden. ", adj.get(root).unwrap()[i], costs.get(adj.get(root).unwrap()[i]).unwrap()-diff)
+			}
+		}
+		// Fehlerfall (Hier hätte ich auch nen Optional nutzen können)
+		return -1;
+	}
+	let mut sum = *costs.get(root).unwrap();
+
+	sum += child_weight.iter().sum();
+
+	sum
+}
