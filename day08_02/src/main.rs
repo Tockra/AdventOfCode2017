fb2e30cb73a38aecf71e9d45a790a2cb946e2cc2
Tim McTa <tim.tannert@tu-dortmund.de>
2017-12-08 10:27:26 +0100

    day08 2017 beendet

--- day08_02/src/main.rs
+++ day08_02/src/main.rs
@@ -1,75 +1,65 @@
-extern crate ndarray;
 
-use ndarray::Array2;
-use ndarray::Axis;
-
+use std::collections::HashMap;
 
 fn main() {
-	let mut screen = Array2::from_elem((50, 6), '.');
+	let mut registers = HashMap::new();
+	let mut max = 0;
 	
 	// Die input Datei wird in die erzeugte Binärdatei eingebunden und als eingabe gelesen. Das dynamische Einlesen von anderen Dateien 
 	// ist nicht notwendig.
 	// Die folgende for-Schleife durchläuft die Datei zeile für Zeile.
 	for line in include_str!("../input.data").lines() {
 		// Datei wird geparset. Das ist hier etwas hässlig, aber es funktioniert ;) 
-		if line.contains("row") {
-			let coords: Vec<&str> = line[13..].split(" ").collect();
-			rotate_row(&mut screen,coords[0].parse::<usize>().unwrap(),coords[coords.len()-1].parse::<usize>().unwrap())
+		
+		let reg = line.split_whitespace().nth(0).unwrap();
+
+		// Register initialisieren
+		init(reg,&mut registers);
+		
+		// Condition vorhanden?
+		if line.contains("if") {
+			let operation = line.split_whitespace().nth(5).unwrap();
+			let left = line.split_whitespace().nth(4).unwrap();
+			let right = line.split_whitespace().nth(6).unwrap();
+			init(left,&mut registers);
+			init(right,&mut registers);
+			
+			let cond: bool = if operation == "==" {*registers.get(left).unwrap() == right.parse::<i32>().unwrap()}
+				else if operation == "!="{*registers.get(left).unwrap() != right.parse::<i32>().unwrap()}
+				else if operation == "<=" {*registers.get(left).unwrap() <= right.parse::<i32>().unwrap()} 
+				else if operation == "<" {*registers.get(left).unwrap() < right.parse::<i32>().unwrap()}
+				else if operation == ">=" {*registers.get(left).unwrap() >= right.parse::<i32>().unwrap()}
+				else {*registers.get(left).unwrap() > right.parse::<i32>().unwrap()};
+			
+			if cond {
+				let val = line.split_whitespace().nth(2).unwrap().parse::<i32>().unwrap();
+				let mut n_v = 0;
+				if line.contains("inc") {
+					n_v = registers.get(&reg.to_string()).unwrap() + val;
+				}
+				else {
+					n_v = registers.get(&reg.to_string()).unwrap() - val;
+				}
+				if n_v > max {
+					max = n_v;
+				}
+				registers.remove(&reg.to_string());
+				registers.insert(reg.to_string(),n_v);
+			}
+
 		}
-		else if line.contains("column") {
-			let coords: Vec<&str> = line[16..].split(" ").collect();
-			rotate_column(&mut screen,coords[0].parse::<usize>().unwrap(),coords[coords.len()-1].parse::<usize>().unwrap());
-		}
-		else if line.contains("rect") {
-			let coords: Vec<&str> = line[5..].split("x").collect();
-			rect(&mut screen,coords[0].parse::<usize>().unwrap(),coords[1].parse::<usize>().unwrap());
-		}
+		
 	}
 	
 	// Variable zum zählen der leuchtenden Pixel. Das Zählen kann mit der Ausgabe verbunden werden
-	let mut count = 0;
 	
-	// Ausgabe
-	for y in 0..screen.len_of(Axis(1)) {
-		for x in 0..screen.len_of(Axis(0)) {
-			if screen[[x,y]] == '#' {
-				count += 1;
-			}
-			print!("{}",screen[[x,y]]);
-		}
-		println!("");
-	}
-	println!("Anzahl der leuchtenden Pixel: {}",count);
+
+	println!("Maximaler Registerwert {}",max);
+	
 }
 
-// Funktion, die ein Rechteck an # erzeugt
-fn rect(screen: &mut Array2<char>,a: usize, b: usize) {
-	for x in 0..a {
-		for y in 0..b {
-			screen[[x,y]] = '#';
-		}
-	}
-}
-// Funktion, die Spalten verschiebt
-fn rotate_column(screen: &mut Array2<char>, x: usize, dis: usize) {	
-	for _ in 0..dis {
-		let copy = screen.clone();
-		
-		screen[[x,0]] = copy[[x,screen.len_of(Axis(1))-1]];
-		for y in 1..copy.len_of(Axis(1)) {
-			screen[[x,y]] = copy[[x,y-1]];
-		}
-	}
-}
-
-// Funktion die Zeilen verschiebt
-fn rotate_row(screen: &mut Array2<char>,y: usize, dis: usize) {
-	for _ in 0..dis {
-		let copy = screen.clone();
-		
-		screen[[0,y]] = copy[[screen.len_of(Axis(0))-1,y]];
-		for x in 1..copy.len_of(Axis(0)) {
-			screen[[x,y]] = copy[[x-1,y]]
-		}
+fn init(register: &str, registers: &mut HashMap<String,i32> ) {
+	if registers.get(register).is_none()  {
+			registers.insert(register.to_string(),0);
 	}
 }
